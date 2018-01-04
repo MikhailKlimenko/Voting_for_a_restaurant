f0e3b7a24e851e5fbb23bc50f053e2ccbedbd4aa
Mikhail <kmgkmgkmg@mail.ru>
2018-01-05 00:53:15 +0300

    implement Restaurant

--- src/main/java/project/model/Restaurant.java
+++ src/main/java/project/model/Restaurant.java
@@ -1,14 +1,45 @@
 package project.model;
 
-public class Restaurant{
+public class Restaurant extends AbstractBaseEntity{
 
-    public Restaurant() {}
+    private String name;
+    private Integer count;
 
-    public Restaurant(Restaurant r) {    }
+    public Restaurant() {
+    }
 
+    public Restaurant(String name) {
+        this(null, name);
+    }
+
+    public Restaurant(Integer id, String name) {
+        super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 
     @Override
     public String toString() {
-        return null;
+        return "Restaurant{" +
+                "id=" + id +
+                "name='" + name + '\'' +
+                ", count=" + count +
+                '}';
     }
+
 }
